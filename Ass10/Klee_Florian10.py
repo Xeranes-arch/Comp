c2df7270ab90c6c28d9916f76cb42216701332b8
Xeranes-arch <florianklee42@gmail.com>
2023-12-14 01:49:10 +0100

    eieiei

--- Ass10/Klee_Florian10.py
+++ Ass10/Klee_Florian10.py
@@ -5,101 +5,164 @@ import matplotlib.pyplot as plt
 from functools import partial
 from functions import *
 
-np.random.seed(420)
-
-# Given System
-A = np.matrix([[7, 3, -1, 2], [3, 8, 1, -4], [-1, 1, 4, -1], [2, -4, -1, 6]]).astype(
-    float
-)
-b = np.matrix([[1], [2], [3], [4]]).astype(float)
-x_0 = np.array([[0.1], [0.1], [0.1], [0.1]])
-sol = np.array([[-1.27619048], [1.87619048], [0.57142857], [2.43809524]])
+np.random.seed()
 
 
-N = 1000
+def given_system():
+    """Algorithms on given System visualized."""
+    A = np.array([[7, 3, -1, 2], [3, 8, 1, -4], [-1, 1, 4, -1], [2, -4, -1, 6]]).astype(
+        float
+    )
+    b = np.matrix([[1], [2], [3], [4]]).astype(float)
+    x_0 = np.array([[0.1], [0.1], [0.1], [0.1]])
+    sol = np.array(np.linalg.solve(A, b))
 
-A, b = genSys(N)
-x_0s = []
-for i in np.arange(10000):
-    arr = np.array(np.matrix(np.ones((i, 1))).astype(float))
-    x_0s.append(arr)
-x_0 = x_0s[N]
+    res = steepest_decent_vis(A, b, x_0)
+    print(type(res), type(sol))
+    print("------------------------------------------\nSD SOLUTION:\n", res)
+    err = max(abs((res - sol) / max(sol))) * 100
+    print(
+        "ERROR of: ",
+        round(float(err[0]), 4),
+        "%\n------------------------------------------",
+    )
+
+    res = CG_vis(A, b, x_0)
+    print("------------------------------------------\nCG SOLUTION:\n", res)
+    err = max(abs((res - sol) / max(sol))) * 100
+    print(
+        "ERROR of: ",
+        round(float(err[0]), 4),
+        "%\n------------------------------------------",
+    )
+    plt.legend(["SD", "CG"])
+    plt.show()
 
 
-# exit()
+def random_system(N):
+    """Algorithms on random System visualized"""
+    A, b, x_0, sol = genSys(N)
 
-# Functions on given System
-res = steepest_decent_vis(A, b, x_0)
-# assert np.isclose(res, sol, atol=0.0001).all()
-# print("ERROR of: ", [float((res[i] - sol[i])[0]) for i in range(len(res))])
+    res = steepest_decent_vis(A, b, x_0, cap=10000)
+    print(type(res), type(sol))
+    print("------------------------------------------\nSD SOLUTION:\n", res)
+    err = max(abs((res - sol) / max(sol))) * 100
+    print(
+        "ERROR of: ",
+        round(float(err[0]), 4),
+        "%\n------------------------------------------",
+    )
 
-# err = []
-# for i in np.arange(1000, step=10):
-#     res = steepest_decent(A, b, x_0, i)
-#     err.append(max([float((res[i] - sol[i])[0]) for i in range(len(res))]))
-
-# plt.plot(np.arange(1000, step=10), err)
-# plt.show()
-
-
-res = CG_vis(A, b, x_0)
-# assert np.isclose(res, sol, atol=0.0001).all()
-# print("ERROR of: ", [float((res[i] - sol[i])[0]) for i in range(len(res))])
+    res = CG_vis(A, b, x_0)
+    print("------------------------------------------\nCG SOLUTION:\n", res)
+    err = max(abs((res - sol) / max(sol))) * 100
+    print(
+        "ERROR of: ",
+        round(float(err[0]), 4),
+        "%\n------------------------------------------",
+    )
+    plt.legend(["SD", "CG"])
+    plt.show()
 
 
-exit()
+def error_analysis_dec():
+    lst = []
+    N = np.arange(3, 10, 1)
+    for i in N:
+        print(i)
+        y = 0
+        n = 0
+        for j in range(100):
+            A, b, x_0, sol = genSys(i)
+            try:
+                res = steepest_decent(A, b, x_0, ctrl=True)
+                y += 1
+            except:
+                n += 1
+        lst.append(y / (y + n))
+    print(N, lst)
+    plt.plot(N, lst)
+    plt.title("Rate of valid matrices that converge within 10000 Iterations")
+    plt.show()
 
-# Generate Systems
-xax = (
-    list(np.arange(2, 11))
-    + list(np.arange(15, 26, 5))
-    + list(np.arange(30, 51, 10))
-    + list(np.arange(60, 101, 20))
-)
-x_0s = []
-for i in xax:
-    arr = np.matrix(np.ones((i, 1))).astype(float)
-    x_0s.append(arr)
-x_0 = x_0s[i]
-# TIMINGS
-res1 = []
-for i, k in enumerate(xax):
-    print("START N=", k, "\n----------------------------------")
-    x_0 = x_0s[i]
-    execution_time = 0
-    for j in range(1):
-        A, b = genSys(k)
-        partial_sd = partial(steepest_decent, A0=A, b0=b, x_0=x_0)
-        execution_time += timeit.timeit(partial_sd, number=1)
-    res1.append(execution_time / 1)
 
-res2 = []
-for i, k in enumerate(xax):
-    print("START N=", k, "\n----------------------------------")
-    x_0 = x_0s[i]
-    execution_time = 0
-    for j in range(10):
-        A, b = genSys(k)
-        partial_cg = partial(CG, A0=A, b0=b, x_0=x_0)
-        execution_time += timeit.timeit(partial_cg, number=1)
-    res2.append(execution_time / 10)
+def error_analysis_cg():
+    lst = []
+    N = np.append(np.append(np.arange(3, 30), np.arange(30, 101, 15)), [200, 500, 1000])
+    for i in N:
+        print(i)
+        y = 0
+        n = 0
+        err = 0
+        for j in range(1000):
+            A, b, x_0, sol = genSys(i)
+            res = CG(A, b, x_0)
+            err += max(abs((res - sol) / max(sol))) * 100
+        lst.append(err / 1000)
+    print(N, lst)
+    plt.plot(N, lst)
+    plt.title("Error of cg in respect to np, at high dimensions")
+    plt.show()
 
-# Complexity comparisons
-n1 = xax
-n2 = [i**2 for i in xax]
-n1 = [i / min(n1) * min(res2) for i in n1]
-n2 = [i / min(n2) * min(res1) for i in n2]
 
-# Plot
-plt.plot(xax, res1)
-plt.plot(xax, res2)
-plt.plot(xax, n1)
-plt.plot(xax, n2)
+def timings():
+    # Generate Systems
+    dims = [2, 3, 4, 10, 20, 50, 100, 200, 500, 1000]
 
-plt.legend(["steepest _decent", "CG", "n", "n^2"])
-plt.title("log log graph of the complexity")
-plt.xlabel("dimension n of matrix")
-plt.ylabel("time in [s]")
-plt.loglog()
+    # TIMINGS
+    # res1 = []
+    # for i, k in enumerate(dims):
+    #     print("START N=", k, "\n----------------------------------")
+    #     execution_time = 0
+    #     c = 0
+    #     for j in range(100):
+    #         A, b, x_0, sol = genSys(k)
+    #         partial_sd = partial(steepest_decent, A0=A, b0=b, x_0=x_0, cap=10000)
+    #         try:
+    #             execution_time += timeit.timeit(partial_sd, number=1)
+    #             c += 1
+    #         except:
+    #             pass
+    #     if c == 0:
+    #         print("no valid matrices")
+    #         exit()
+    #     res1.append(execution_time / c)
 
-plt.show()
+    res2 = []
+    for i, k in enumerate(dims):
+        print("START N=", k, "\n----------------------------------")
+        execution_time = 0
+        for j in range(10):
+            A, b, x_0, sol = genSys(k)
+            partial_cg = partial(CG, A0=A, b0=b, x_0=x_0)
+            execution_time += timeit.timeit(partial_cg, number=1)
+        res2.append(execution_time / 10)
+
+    # Complexity comparisons
+    # n1 = dims
+    # n2 = [i**2 for i in dims]
+    n1 = [i / min(n1) * min(res2) for i in n1]
+    # n2 = [i / min(n2) * min(res1) for i in n2]
+
+    # Plot
+    # plt.plot(dims, res1)
+    plt.plot(dims, res2)
+    # plt.plot(dims, n1)
+    # plt.plot(dims, n2)
+
+    plt.legend(["steepest _decent", "CG", "n", "n^2"])
+    plt.title("log log graph of the complexity")
+    plt.xlabel("dimension n of matrix")
+    plt.ylabel("time in [s]")
+    plt.loglog()
+
+    plt.show()
+
+
+# SETTING WHICH PARTS TO RUN
+
+# given_system()
+# random_system(5)
+# error_analysis_dec()
+error_analysis_cg()
+# timings()
--- Ass10/functions.py
+++ Ass10/functions.py
@@ -8,7 +8,7 @@ def func(A, b, x):
     )
 
 
-def steepest_decent_vis(A0, b0, x_0):
+def steepest_decent_vis(A0, b0, x_0, cap=10000):
     A = np.matmul(np.transpose(A0), A0)
     b = np.matmul(np.transpose(A0), b0)
 
@@ -17,7 +17,6 @@ def steepest_decent_vis(A0, b0, x_0):
     k = 0
     while True:
         x_o = x_n.copy()
-
         r_n = b - np.matmul(A, x_n)
         alph = np.matmul(np.transpose(r_n), r_n) / np.matmul(
             np.transpose(r_n), np.matmul(A, r_n)
@@ -27,25 +26,40 @@ def steepest_decent_vis(A0, b0, x_0):
         xs = np.hstack((xs, x_n))
 
         if (
-            all(abs(i) <= 0.0001 for i in r_n)
-            or max(abs(np.array(x_o) - np.array(x_n))) < 0.0001
+            all(abs(i) <= 0.000001 for i in r_n)
+            or max(abs(np.array(x_o) - np.array(x_n))) < 0.000001
         ):
-            print("SOLUTION:\n", x_n)
             plt.plot(np.array(xs[0, :]), np.array(xs[1, :]))
-            plt.scatter(np.array(xs[0, -1]), np.array(xs[1, -1]), marker="+", c="r")
+            plt.scatter(
+                np.array(xs[0, -1]),
+                np.array(xs[1, -1]),
+                marker="+",
+                c="r",
+                label="_nolegend_",
+            )
             plt.title((f"SOLUTION for N = {len(b)} with {k} STEPS"))
             # plt.show()
             break
-        if not k % 1000:
+        if not k % 1000 and k != 0:
             print(k, "STEPS")
-        if k == 100000:
-            exit()
+            plt.scatter(
+                np.array(xs[0, -1]),
+                np.array(xs[1, -1]),
+                marker="+",
+                c="r",
+                label="_nolegend_",
+            )
+            plt.title((f"SOLUTION for N = {len(b)} with {k} STEPS"))
+        if k == cap:
+            plt.plot(np.array(xs[0, :]), np.array(xs[1, :]))
+            print("cap triggered")
+            break
 
         k += 1
     return x_n
 
 
-def steepest_decent(A0, b0, x_0, nr=1000):
+def steepest_decent(A0, b0, x_0, cap=10000, ctrl=False):
     A = np.matmul(A0.T, A0)
     b = np.matmul(A0.T, b0)
     x_n = x_0.copy()
@@ -60,26 +74,29 @@ def steepest_decent(A0, b0, x_0, nr=1000):
         )
         alph = alph.item()
         x_n += alph * r_n
-        # if k == nr:
-        #     print(k, "Iterations")
-        #     break
         if (
             all(abs(i) <= 0.000001 for i in r_n)
             or max(abs(np.array(x_o) - np.array(x_n))) < 0.000001
         ):
-            print(k, "Iterations")
+            break
+        if k == cap:
+            if ctrl:
+                raise RuntimeError
+            print("cap triggered")
             break
         k += 1
     return x_n
 
 
 def CG(A0, b0, x_0):
+    """This guy performs so well, Error wise, that I'm using it as a measure for the Error of SD."""
     A = np.matmul(np.transpose(A0), A0)
     b = np.matmul(np.transpose(A0), b0)
 
     r_n = b - np.matmul(A, x_0)
     if all(np.isclose(r_n, 0, atol=0.00001)):
         return x_0
+
     p_n = r_n
     x_n = x_0.copy()
     n = 0
@@ -88,17 +105,19 @@ def CG(A0, b0, x_0):
             np.matmul(np.transpose(r_n), r_n)
             / np.matmul(np.transpose(p_n), np.matmul(A, p_n))
         ).item()
-        x_n += alph * p_n
 
+        x_n += alph * p_n
         r_nn = r_n - alph * np.matmul(A, p_n)
-        if all(np.isclose(r_nn, 0)):
-            print(n + 1, "Iterations")
-            break
+
         beta = (
             np.matmul(np.transpose(r_nn), r_nn) / np.matmul(np.transpose(r_n), r_n)
         ).item()
         p_n = r_nn + beta * p_n
         r_n = r_nn
+
+        if n == len(b):
+            break
+
         n += 1
     return x_n
 
@@ -125,13 +144,16 @@ def CG_vis(A0, b0, x_0):
         r_nn = r_n - alph * np.matmul(A, p_n)
         # if all(np.isclose(r_nn, 0)):
         if n == len(b):
-            print(n, "Iterations")
             plt.plot(np.array(xs[0, :]), np.array(xs[1, :]))
-            plt.scatter(np.array(xs[0, -1]), np.array(xs[1, -1]), marker="+", c="r")
-            plt.title((f"SOLUTION for N = {len(b)} with {n} STEPS"))
-            plt.show()
-
+            plt.scatter(
+                np.array(xs[0, -1]),
+                np.array(xs[1, -1]),
+                marker="+",
+                c="r",
+                label="_nolegend_",
+            )
             break
+
         beta = (
             np.matmul(np.transpose(r_nn), r_nn) / np.matmul(np.transpose(r_n), r_n)
         ).item()
@@ -142,34 +164,11 @@ def CG_vis(A0, b0, x_0):
 
 
 def genSys(i):
-    A = np.matrix(np.random.randint(-9, 9, size=(i, i))).astype(float)
+    A = (np.random.randint(-9, 9, size=(i, i))).astype(float)
+    while np.linalg.matrix_rank(A) < i:
+        A = (np.random.randint(-9, 9, size=(i, i))).astype(float)
+    # A = np.matrix(np.random.randint(-9, 9, size=(i, i))).astype(float)
     b = np.matrix(np.random.randint(-9, 9, size=(i, 1))).astype(float)
-    return A, b
-
-
-def updaterow_U(M, L, U, i, n):
-    for j in range(i, n):
-        U[i, j] = M[i, j]
-        for k in range(i):
-            U[i, j] -= L[i, k] * U[k, j]
-    return U
-
-
-def updatecol_L(M, L, U, i, n):
-    for j in range(i + 1, n):
-        L[j, i] = M[j, i]
-        for k in range(i):
-            L[j, i] -= L[j, k] * U[k, i]
-        L[j, i] /= U[i, i]
-    return L
-
-
-def LU(M):
-    "The main function. Rather self explanatory and compact, but not readable. Math. It is what it is."
-    n = np.shape(M)[0]
-    L = np.identity(n)
-    U = np.zeros_like(M)
-    for i in range(n):
-        U = updaterow_U(M, L, U, i, n)
-        L = updatecol_L(M, L, U, i, n)
-    return L, U
+    x_0 = np.array(np.matrix(np.ones((i, 1))).astype(float))
+    sol = np.array(np.linalg.solve(A, b))
+    return A, b, x_0, sol
